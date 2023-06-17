0c72ebb71a9371905cf3deb9f32b9e8047fe0170
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-06-18 00:45:15 +0800

    timestamp: 2023-06-18 00:45:15

--- 01-simple-boot/boot.s
+++ 01-simple-boot/boot.s
@@ -1,22 +1,31 @@
-[bits 16]    ; 设置 16 位汇编
-[org 0x7c00] ; 设置起始地址
+[bits 16]                       ; 设置 16 位汇编
+[org 0x7c00]                    ; 设置起始地址
 
-init:
-  mov SI, msg  ; 读取 msg 地址到 SI
-  mov AH, 0x0e ; teletype
+_start:
+    jmp main
 
-print_char:
-  lodsb          ; 从 SI 中读取 1 byte 到 AL, 然后 SI++
-  cmp AL, 0      ; AL 是否为 0
-  je done        ; if AL == 0, jump to "done"
-  int 0x10       ; 中断向屏幕输出一个字符
-  jmp print_char ; 循环读取下一个字符
-done:
-  hlt            ; 停止
+putstr:
+    pusha
+    mov AH, 0x0e                ; 设置中断为输出一个字符, teletype
+putc_begin:
+    lodsb                       ; 从 SI 中读取 1 byte 到 AL, 然后 SI++
+    cmp AL, 0                   ; AL 是否为 0
+    je putc_end                 ; if AL == 0, 函数返回
+    int 0x10                    ; 中断向屏幕输出一个字符
+    jmp putc_begin              ; 循环读取下一个字符
+putc_end:
+    popa
+    ret
+
+main:
+    mov SI, msg                 ; 读取 msg 地址到 SI
+    call putstr
+    hlt                         ; 系统休眠
 
 msg:
-    db "Hello world!",
-    db 0 ; we need to explicitely put the zero byte here
+    db 'Hello, Jeffrey!', 0x0d, 0x0a
+    db '  Starting...', 0x0d, 0x0a
+    db 0
 
-times 510-($-$$) db 0 ; 填充多余的 510 个字节为零值
-dw 0xaa55             ; BIOS 结束校验码
+    times 510-($-$$) db 0       ; 填充多余的 510 个字节为零值
+    dw 0xaa55                   ; BIOS 结束校验码

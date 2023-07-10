38a5919881da05a9d55f10b317b6decf80713771
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-10 21:24:21 +0800

    update

--- 03-inline-asm/add.c
+++ 03-inline-asm/add.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-#define get_seg_byte(seg, addr) ({                                      \
-            register char __res;                                        \
-            __asm__("push %%fs; mov %%ax, %%fs; movb %%fs:%2, %%al; pop %%fs;" \
-                    : "=a" (__res)                                      \
-                    : "0" (seg), "m" (*(addr)));                        \
+#define get_seg_byte(seg, addr) ({                  \
+            register char __res;                    \
+            __asm__("push %%fs\n\t"                 \
+                    "mov %%ax, %%fs\n\t"            \
+                    "movb %%fs:%2, %%al\n\t"        \
+                    "pop %%fs"                      \
+                    : "=a" (__res)                  \
+                    : "0" (seg), "m" (*(addr)));    \
             __res;})
 
 

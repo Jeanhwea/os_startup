763c46f568ae96d85858796edb13b98ffa882f17
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-11 14:19:04 +0800

    update source codes

---
+++ 04-gcc-funcall/my_proc.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    char buf[1024];
+    int a, b, ans;
+
+    char *mystr = "Calculating ...\n";
+    char *emsg = "Error in handling\n";
+
+    a = 5, b = 10;
+    mywrite(1, mystr, strlen(mystr));
+    if (myadd(a, b, &ans)) {
+        sprintf(buf, "The answer is %d\n", ans);
+        mywrite(1, buf, strlen(buf));
+    } else {
+        mywrite(1, emsg, strlen(emsg));
+    }
+
+    return 0;
+}

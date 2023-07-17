7ddb86b3a6be31ea61b1d980298922c03871aaa4
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-17 09:20:05 +0800

    timestamp: 2023-07-17 09:20:05

--- 11-first-boot/boot.s
+++ 11-first-boot/boot.s
@@ -9,23 +9,23 @@ SYSLEN  = 17                    ! sectors occupied.
 
 entry start
 start:
-    jmpi go, #BOOTSEG
-go:	mov  ax, cs
-    mov  ds, ax
-    mov	 ss, ax
-    mov	 sp, #0x400             ! arbitrary value >>512
+    jmpi    go, #BOOTSEG
+go: mov     ax, cs
+    mov     ds, ax
+    mov     ss, ax
+    mov     sp, #0x400             ! arbitrary value >>512
 
 ! ok, we've written the message, now
 load_system:
-    mov	dx, #0x0000
-    mov	cx, #0x0002
-    mov	ax, #SYSSEG
-    mov	es, ax
-    xor	bx, bx
-    mov	ax, #0x200+SYSLEN
-    int 0x13
-    jnc	ok_load
-die:	jmp	die
+    mov     dx, #0x0000
+    mov     cx, #0x0002
+    mov     ax, #SYSSEG
+    mov     es, ax
+    xor     bx, bx
+    mov     ax, #0x200+SYSLEN
+    int     0x13
+    jnc     ok_load
+die:jmp     die
 
 ! now we want to move to protected mode ...
 ok_load:
@@ -41,29 +41,29 @@ ok_load:
     movw
     mov     ax, #BOOTSEG
     mov     ds, ax
-    lidt	idt_48              ! load idt with 0,0
-    lgdt	gdt_48              ! load gdt with whatever appropriate
+    lidt    idt_48              ! load idt with 0,0
+    lgdt    gdt_48              ! load gdt with whatever appropriate
 
 ! absolute address 0x00000, in 32-bit protected mode.
     mov     ax, #0x0001         ! protected mode (PE) bit
-    lmsw	ax                  ! This is it!
-    jmpi	0, 8                ! jmp offset 0 of segment 8 (cs)
+    lmsw    ax                  ! This is it!
+    jmpi    0, 8                ! jmp offset 0 of segment 8 (cs)
 
-gdt:	.word	0, 0, 0, 0		! dummy
+gdt:    .word    0, 0, 0, 0        ! dummy
 
-    .word	0x07FF              ! 8Mb - limit=2047 (2048*4096=8Mb)
-    .word	0x0000              ! base address=0x00000
-    .word	0x9A00              ! code read/exec
-    .word	0x00C0              ! granularity=4096, 386
+    .word    0x07FF              ! 8Mb - limit=2047 (2048*4096=8Mb)
+    .word    0x0000              ! base address=0x00000
+    .word    0x9A00              ! code read/exec
+    .word    0x00C0              ! granularity=4096, 386
 
-    .word	0x07FF              ! 8Mb - limit=2047 (2048*4096=8Mb)
-    .word	0x0000              ! base address=0x00000
-    .word	0x9200              ! data read/write
-    .word	0x00C0              ! granularity=4096, 386
+    .word    0x07FF              ! 8Mb - limit=2047 (2048*4096=8Mb)
+    .word    0x0000              ! base address=0x00000
+    .word    0x9200              ! data read/write
+    .word    0x00C0              ! granularity=4096, 386
 
-idt_48: .word	0               ! idt limit=0
-    .word	0, 0                ! idt base=0L
-gdt_48: .word	0x7ff           ! gdt limit=2048, 256 GDT entries
-    .word	0x7c00+gdt, 0       ! gdt base = 07xxx
+idt_48: .word    0               ! idt limit=0
+    .word    0, 0                ! idt base=0L
+gdt_48: .word    0x7ff           ! gdt limit=2048, 256 GDT entries
+    .word    0x7c00+gdt, 0       ! gdt base = 07xxx
 .org 510
     .word   0xAA55

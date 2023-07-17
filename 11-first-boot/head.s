f3742825f3d9891890840a30c53b6bb678b39de5
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-17 08:39:14 +0800

    timestamp: 2023-07-17 08:39:14

--- 11-first-boot/head.s
+++ 11-first-boot/head.s
@@ -10,123 +10,123 @@ LDT1_SEL	= 0x38
 .global startup_32
 .text
 startup_32:
-    movl $0x10,%eax
-    mov %ax,%ds
-#	mov %ax,%es
-    lss init_stack,%esp
+    movl    $0x10, %eax
+    mov     %ax, %ds
+#	mov     %ax, %es
+    lss     init_stack, %esp
 
 # setup base fields of descriptors.
-    call setup_idt
-    call setup_gdt
-    movl $0x10,%eax		# reload all the segment registers
-    mov %ax,%ds		# after changing gdt.
-    mov %ax,%es
-    mov %ax,%fs
-    mov %ax,%gs
-    lss init_stack,%esp
+    call    setup_idt
+    call    setup_gdt
+    movl    $0x10, %eax             # reload all the segment registers
+    mov     %ax, %ds                # after changing gdt.
+    mov     %ax, %es
+    mov     %ax, %fs
+    mov     %ax, %gs
+    lss     init_stack, %esp
 
 # setup up timer 8253 chip.
-    movb $0x36, %al
-    movl $0x43, %edx
-    outb %al, %dx
-    movl $11930, %eax        # timer frequency 100 HZ
-    movl $0x40, %edx
-    outb %al, %dx
-    movb %ah, %al
-    outb %al, %dx
+    movb    $0x36, %al
+    movl    $0x43, %edx
+    outb    %al, %dx
+    movl    $11930, %eax            # timer frequency 100 HZ
+    movl    $0x40, %edx
+    outb    %al, %dx
+    movb    %ah, %al
+    outb    %al, %dx
 
 # setup timer & system call interrupt descriptors.
-    movl $0x00080000, %eax
-    movw $timer_interrupt, %ax
-    movw $0x8E00, %dx
-    movl $0x08, %ecx              # The PC default timer int.
-    lea idt(,%ecx,8), %esi
-    movl %eax,(%esi)
-    movl %edx,4(%esi)
-    movw $system_interrupt, %ax
-    movw $0xef00, %dx
-    movl $0x80, %ecx
-    lea idt(,%ecx,8), %esi
-    movl %eax,(%esi)
-    movl %edx,4(%esi)
+    movl    $0x00080000, %eax
+    movw    $timer_interrupt, %ax
+    movw    $0x8E00, %dx
+    movl    $0x08, %ecx             # The PC default timer int.
+    lea     idt(,%ecx,8), %esi
+    movl    %eax, (%esi)
+    movl    %edx, 4(%esi)
+    movw    $system_interrupt, %ax
+    movw    $0xef00, %dx
+    movl    $0x80, %ecx
+    lea     idt(,%ecx,8), %esi
+    movl    %eax, (%esi)
+    movl    %edx, 4(%esi)
 
 # unmask the timer interrupt.
-#	movl $0x21, %edx
-#	inb %dx, %al
-#	andb $0xfe, %al
-#	outb %al, %dx
+#	movl    $0x21, %edx
+#	inb     %dx, %al
+#	andb    $0xfe, %al
+#	outb    %al, %dx
 
 # Move to user mode (task 0)
     pushfl
-    andl $0xffffbfff, (%esp)
+    andl    $0xffffbfff, (%esp)
     popfl
-    movl $TSS0_SEL, %eax
-    ltr %ax
-    movl $LDT0_SEL, %eax
-    lldt %ax
-    movl $0, current
+    movl    $TSS0_SEL, %eax
+    ltr     %ax
+    movl    $LDT0_SEL, %eax
+    lldt    %ax
+    movl    $0, current
     sti
-    pushl $0x17
-    pushl $init_stack
+    pushl   $0x17
+    pushl   $init_stack
     pushfl
-    pushl $0x0f
-    pushl $task0
+    pushl   $0x0f
+    pushl   $task0
     iret
 
 /****************************************/
 setup_gdt:
-    lgdt lgdt_opcode
+    lgdt    lgdt_opcode
     ret
 
 setup_idt:
-    lea ignore_int,%edx
-    movl $0x00080000,%eax
-    movw %dx,%ax		/* selector = 0x0008 = cs */
-    movw $0x8E00,%dx	/* interrupt gate - dpl=0, present */
-    lea idt,%edi
-    mov $256,%ecx
+    lea     ignore_int, %edx
+    movl    $0x00080000, %eax
+    movw    %dx, %ax            /* selector = 0x0008 = cs */
+    movw    $0x8E00, %dx        /* interrupt gate - dpl=0, present */
+    lea     idt, %edi
+    mov     $256, %ecx
 rp_sidt:
-    movl %eax,(%edi)
-    movl %edx,4(%edi)
-    addl $8,%edi
-    dec %ecx
-    jne rp_sidt
-    lidt lidt_opcode
+    movl    %eax, (%edi)
+    movl    %edx, 4(%edi)
+    addl    $8,%edi
+    dec     %ecx
+    jne     rp_sidt
+    lidt    lidt_opcode
     ret
 
 # -----------------------------------
 write_char:
-    push %gs
-    pushl %ebx
-#	pushl %eax
-    mov $SCRN_SEL, %ebx
-    mov %bx, %gs
-    movl scr_loc, %ebx
-    shl $1, %ebx
-    movb %al, %gs:(%ebx)
-    shr $1, %ebx
-    incl %ebx
-    cmpl $2000, %ebx
-    jb 1f
-    movl $0, %ebx
-1:	movl %ebx, scr_loc
-#	popl %eax
-    popl %ebx
-    pop %gs
+    push    %gs
+    pushl   %ebx
+#	pushl   %eax
+    mov     $SCRN_SEL, %ebx
+    mov     %bx, %gs
+    movl    scr_loc, %ebx
+    shl     $1, %ebx
+    movb    %al, %gs:(%ebx)
+    shr     $1, %ebx
+    incl    %ebx
+    cmpl    $2000, %ebx
+    jb      1f
+    movl    $0, %ebx
+1:	movl    %ebx, scr_loc
+#	popl    %eax
+    popl    %ebx
+    pop     %gs
     ret
 
 /***********************************************/
 /* This is the default interrupt "handler" :-) */
 .align 2
 ignore_int:
-    push %ds
-    pushl %eax
-    movl $0x10, %eax
-    mov %ax, %ds
-    movl $67, %eax            /* print 'C' */
-    call write_char
-    popl %eax
-    pop %ds
+    push    %ds
+    pushl   %eax
+    movl    $0x10, %eax
+    mov     %ax, %ds
+    movl    $67, %eax               /* print 'C' */
+    call    write_char
+    popl    %eax
+    pop     %ds
     iret
 
 /* Timer interrupt handler */

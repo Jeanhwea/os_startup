24e565a33342d831113992d5a50fe037b3a4383a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-21 12:14:00 +0800

    add

--- 10-gdb-as86/boot.s
+++ 10-gdb-as86/boot.s
@@ -2,16 +2,11 @@ BOOTSEG = 0x07c0
 
 entry start
 start:
-
-go: mov     ax, #3
-    dec     ax
-    dec     ax
-    dec     ax
     jmpi    go, #BOOTSEG
-
-say:mov     ah, #0x0e
+go: mov     ah, #0x0e
     mov     al, #0x58   ! char 'X'
     int     0x10        ! print char
+    jmp     go
 die:jmp     die
 
 .org 510

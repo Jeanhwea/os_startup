e801fc3e2f46b7ad083e7f3c49682e86b6504cd8
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-22 18:55:30 +0800

    add

--- 09-gdb-nasm/boot.asm
+++ 09-gdb-nasm/boot.asm
@@ -1,12 +1,14 @@
 [org 0x7c00]
-    mov ax, 1
-    inc ax
-    inc ax
-    inc ax
-    xor ax, ax
-    inc ax
-    inc ax
-die:jmp die
+
+BOOTSEG equ 0x07c0
+
+start:
+    jmpi    go, BOOTSEG
+go: mov     ah, 0x0e
+    mov     al, 0x58            ; char 'X'
+    int     0x10                ; print char
+    jmp     go
+die:jmp     die
 
 times 510-($-$$) db 0
 dw 0xaa55
